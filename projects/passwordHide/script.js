497e884e7ba11592c00b2e468ebbbb5d2a7e51d2
karan shah <119512863+karanshah675@users.noreply.github.com>
2024-03-19 16:14:01 +0530

    Update script.js

--- projects/passwordHide/script.js
+++ projects/passwordHide/script.js
@@ -2,9 +2,8 @@ let pass = document.querySelector("#pass");
 let eye = pass.nextElementSibling;
 let str = stringg();
  
-function show() {
-  alert(str);
-  if (pass.type == "password") {
+function show() { 
+ if (pass.type == "password") {
     pass.type = "text";
     eye.setAttribute("class", "fa-solid fa-eye-slash");
   } else {
