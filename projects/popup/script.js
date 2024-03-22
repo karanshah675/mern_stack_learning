fa9dd81a8f81a2a5239e483af1016cdbb7ba01f3
karan shah <119512863+karanshah675@users.noreply.github.com>
2024-03-22 20:45:55 +0530

    Add files via upload

---
+++ projects/popup/script.js
@@ -0,0 +1,11 @@
+let openPopUp = document.getElementById("submit");
+let popup = document.querySelector(".popUp");
+let closePopUp = document.querySelector(".pop-btn");
+
+function openpopup(){
+    popup.classList.add("openpopup");
+}
+function closepopup(){
+    popup.classList.remove("openpopup");
+}
+
