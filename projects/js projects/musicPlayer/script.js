9a73bf55f2cc981cdd42fe8d7a2442af6c3d121e
karanshah675 <shahkaran0675@gmail.com>
2024-03-31 22:18:52 +0530

    heart action and progress range added

--- projects/js projects/musicPlayer/script.js
+++ projects/js projects/musicPlayer/script.js
@@ -1,12 +1,29 @@
 let progress = document.getElementById("myRange");
 let song = document.getElementById("audio");
 let cntrlIcon = document.querySelector(".play").firstElementChild;
-
+let player = document.getElementById("range");
+let favBtn = document.querySelector(".fav-heart");
 
 song.addEventListener("onloadedmetadata", () => {
   progress.max = song.duration;
   progress.value = song.currenTime;
 });
+
+favBtn.addEventListener("click", () => {
+  if (!favBtn.classList.contains("fa-solid")) {
+    favBtn.style.color = "#1dd05d";
+  } else {
+    favBtn.style.color = "white";
+  }
+  if (favBtn.classList.contains("fa-regular")) {
+    favBtn.classList.add("fa-solid");
+    favBtn.classList.remove("fa-regular");
+  } else {
+    favBtn.classList.remove("fa-solid");
+    favBtn.classList.add("fa-regular");
+  }
+});
+
 cntrlIcon.addEventListener("click", () => {
   if (cntrlIcon.classList.contains("fa-pause")) {
     song.pause();
@@ -21,6 +38,8 @@ cntrlIcon.addEventListener("click", () => {
 if (song.play()) {
   setInterval(() => {
     progress.value = song.currentTime;
+    var songTime = progress.value * 2.9;
+    player.style.width = songTime + "px";
   }, 500);
 }
 progress.onchange = () => {
