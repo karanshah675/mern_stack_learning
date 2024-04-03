135a2432285d476dab79fa9e5c3c7d2b933df3a9
karanshah675 <shahkaran0675@gmail.com>
2024-04-03 14:21:09 +0530

    style editted

--- projects/js projects/notesApp/script.js
+++ projects/js projects/notesApp/script.js
@@ -1,37 +1,16 @@
 let con = document.querySelector(".container");
 let newNotes = document.getElementById("createNotes");
-let bold = document.querySelector("#bold");
-let italic = document.querySelector("#italic");
-let underLine = document.querySelector("#underline");
+let heading = document.querySelector(".heading");
 let deletee = document.querySelector(".delete");
 let divForNotes = document.querySelector(".notes");
 
-newNotes.addEventListener("click", () => {
+heading.addEventListener("keypress", () => {
+  heading.style.color = "black;";
+});
+newNotes.addEventListener("change", () => {
   let p = document.createElement("p");
   p.className = "textarea";
   p.setAttribute("contenteditable", "true");
-  let tools = document.createElement("div");
-  tools.className = "tools";
-  tools.innerHTML = `
-            <p id="bold">B</p>
-            <p id="italic">I</p>
-            <p id="underline">U</p>
-            <img src="delete.png" class="delete" width="30" alt="" srcset="" />
-        `;
-  divForNotes.appendChild(p);
- 
-});
 
-bold.addEventListener("click", (e) => {
-  e.target.parentElement.classList.add("bold");
-});
-italic.addEventListener("click", (e) => {
-  e.target.parentElement.parentElement.firstElementChildclassList.toggle(
-    "italic"
-  );
-});
-bold.addEventListener("click", (e) => {
-  e.target.parentElement.parentElement.firstElementChild.classList.toggle(
-    "bold"
-  );
+  divForNotes.appendChild(p);
 });
