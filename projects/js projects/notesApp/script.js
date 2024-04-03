1d57aad7c2c27e32102a11842e8e69e0bca662da
karanshah675 <shahkaran0675@gmail.com>
2024-04-03 16:57:26 +0530

    margin added

--- projects/js projects/notesApp/script.js
+++ projects/js projects/notesApp/script.js
@@ -1,15 +1,10 @@
 let con = document.querySelector(".container");
 let newNotes = document.getElementById("createNotes");
-let heading = document.querySelector(".heading");
-let deletee = document.querySelector(".delete");
 let divForNotes = document.querySelector(".notes");
 
-heading.addEventListener("keypress", () => {
-  heading.style.color = "black;";
-});
 newNotes.addEventListener("click", () => {
   let note = document.createElement("div");
-  note.className="note"
+  note.className = "note";
   note.innerHTML = `<input type="text" class="heading" placeholder="heading" />
           <p
             class="textarea"
@@ -18,6 +13,6 @@ newNotes.addEventListener("click", () => {
             contenteditable="true"
             placeholder="write something..."
           ></p>`;
-
+ 
   divForNotes.appendChild(note);
 });
