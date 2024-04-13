ed6a1e6b00810ce257df235d81fba7bacead9f8f
karanshah675 <shahkaran0675@gmail.com>
2024-04-13 12:59:25 +0530

    script and style editted

--- javascript/js projects/virtualKeyBoard/script.js
+++ javascript/js projects/virtualKeyBoard/script.js
@@ -1,44 +1,73 @@
 let display = document.querySelector(".output");
-display.value = "karansss";
 let btn = document.querySelectorAll(".btn");
 let cap = false;
 let shift = false;
+let audio = new Audio("keypress.mp3");
+
+const capsCheck = (ele) => {
+  if (cap == false) {
+    cap = true;
+    ele.target.style.backgroundColor = "#f3351e";
+    ele.target.style.color = "#fff";
+  } else {
+    cap = false;
+    ele.target.style.backgroundColor = "#FB6D48";
+    ele.target.style.color = "#000";
+  }
+};
+
+const shiftCheck = (ele) => {
+  if (shift == false) {
+    ele.target.style.backgroundColor = "#135D66";
+    ele.target.style.color = "#fff";
+    shift = true;
+  } else {
+    shift = false;
+    ele.target.style.backgroundColor = "#77B0AA";
+    ele.target.style.color = "#000";
+  }
+};
+const clickAdd = (ele) => {
+  if (!ele.classList.contains("click")) {
+    ele.classList.add("click");
+    audio.play();
+  }
+  setTimeout(() => {
+    ele.classList.remove("click");
+  }, 100);
+};
 
 btn.forEach((e) => {
   e.addEventListener("click", (ele) => {
     if (e.value == "enter") {
       display.value += "\n";
+      clickAdd(e);
     } else if (e.value == "caps") {
-      if (cap == false) {
-        cap = true;
-        ele.target.style.backgroundColor = "#f3351e";
-        ele.target.style.color = "#fff";
-      } else {
-        cap = false;
-        ele.target.style.backgroundColor = "#fff";
-        ele.target.style.color = "#000";
-      }
+      capsCheck(ele);
+      clickAdd(e);
     } else if (e.value == "shift") {
-      if (shift == false) {
-        ele.target.style.backgroundColor = "#f3351e";
-        ele.target.style.color = "#fff";
-        shift = true;
-      } else {
-        shift = false;
-        ele.target.style.backgroundColor = "#fff";
-        ele.target.style.color = "#000";
-      }
+      shiftCheck(ele);
+      clickAdd(e);
     } else if (e.value == "back") {
       display.value = display.value.substring(0, display.value.length - 1);
-    } else if(ele.target.classList.contains("space")){
-        display.value+=" ";
-    }else {
-      if (cap == true) {
-        display.value += e.value.toUpperCase();
-      } else if (shift == true && e.parentElement.classList.contains("extra")) {
+      clickAdd(e);
+    } else if (ele.target.classList.contains("space")) {
+      display.value += " ";
+      clickAdd(e);
+    }else if(e.classList.contains("meta")){
+      clickAdd(e);
+      return;
+    } 
+    else {
+      if (shift == true && e.parentElement.classList.contains("extra")) {
         display.value += e.nextElementSibling.innerHTML;
+        clickAdd(e);
+      } else if (cap == true) {
+        display.value += e.value.toUpperCase();
+        clickAdd(e);
       } else {
         display.value += e.value;
+        clickAdd(e);
       }
     }
   });
