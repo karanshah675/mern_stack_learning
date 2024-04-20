5180a35fb1febc4f7659ac7043de64b42b733958
karanshah675 <shahkaran0675@gmail.com>
2024-04-20 13:24:14 +0530

    project completed

--- javascript/js projects/solarSystem/script.js
+++ javascript/js projects/solarSystem/script.js
@@ -3,46 +3,57 @@ const caclBtn = document.querySelector("#calc-btn");
 const mass = document.querySelector("#mass");
 const nameOfPlanet = document.getElementById("nameOfPlanet");
 const plantImg = document.querySelector(".planet-image");
+const description = document.querySelector(".description");
+
 const planets = {
-  mercury: {
-    mass: 3.3e23, // Mass in kg
-    radius: 2440e3,
-    iamge: "images/mercury.jpg", // Radius in meters
-  },
   venus: {
     mass: 4.87e24,
     radius: 6052e3,
     iamge: "images/venus.jpg",
+    bgColor: "rgba(180, 134, 64, 0.699)",
+    color: "#1c1c1c",
   },
   earth: {
     mass: 5.97e24,
     radius: 6371e3,
     iamge: "images/earth.jpg",
+    bgColor: "  rgba(15, 16, 56, 0.699)",
+    color: "#fff",
   },
   mars: {
     mass: 6.42e23,
     radius: 3390e3,
     iamge: "images/mars.jpg",
+    bgColor: "  rgba(163, 63, 1,0.699)",
+    color: "#fff",
   },
   jupiter: {
     mass: 1.89e27,
     radius: 69911e3,
     iamge: "images/jupiter.jpg",
+    bgColor: "  rgba(65, 45, 22,0.699)",
+    color: "#fff",
   },
   saturn: {
     mass: 5.68e26,
     radius: 60268e3,
     iamge: "images/saturn.jpg",
+    bgColor: "  rgba(181, 167, 130,0.699)",
+    color: "#1c1c1c",
   },
   uranus: {
     mass: 8.68e25,
     radius: 25559e3,
     iamge: "images/uranus.jpg",
+    bgColor: "  rgba(150, 198, 198,0.699)",
+    color: "#1c1c1c",
   },
   neptune: {
     mass: 1.02e26,
     radius: 24622e3,
     iamge: "images/neptune.jpg",
+    bgColor: "  rgba(30, 29, 169,0.699)",
+    color: "#fff",
   },
 };
 
@@ -66,13 +77,42 @@ function calculateWeight(planetName, mass) {
 }
 
 caclBtn.addEventListener("click", () => {
-  let massOfObject = Math.round(
-    calculateWeight(select.value.toLowerCase(), mass.value)
-  );
-  nameOfPlanet.innerHTML = `${select.value} ${massOfObject} n `;
-  for (const key in planets) {
-    if (key == select.value.toLowerCase()) {
-      plantImg.src = `images/${key}.jpg`;
-    }
+ 
+  if (description.classList.contains("animationStart")) {
+    description.classList.remove("animationStart");
+  }
+  if (mass.value !== "" && select.value !== "none") {
+    let bg = planets[select.value.toLowerCase()];
+    let massOfObject = Math.round(
+      calculateWeight(select.value.toLowerCase(), mass.value)
+    );
+    plantImg.src = `images/${select.value.toLowerCase()}.jpg`;
+    setTimeout(() => {
+      nameOfPlanet.innerHTML = `${select.value} ${massOfObject} n `;
+      nameOfPlanet.previousElementSibling.innerHTML = `the weight of the object on `;
+      nameOfPlanet.parentElement.parentElement.style.background = bg.bgColor;
+      nameOfPlanet.parentElement.parentElement.style.color = bg.color;
+      // description.classList.animation = "anim 2s forwards linear";
+      description.classList.add("animationStart");
+      if (bg.color === "#1c1c1c") {
+        description.firstElementChild.style.backgroundColor = "#0000002f";
+      } else {
+        description.firstElementChild.style.backgroundColor = "#d4d2d22f";
+      }
+    }, 100);
+  } else if (select.value !== "none" && mass.value === "") {
+    plantImg.src = `images/galaxy.jpg`;
+    alert("please enter mass");
+  } else if (select.value === "none" && mass.value !== "") {
+    plantImg.src = `images/galaxy.jpg`;
+    alert("please select the planet");
+  } else {
+    plantImg.src = `images/galaxy.jpg`;
+    alert("please enter mass and select any planet.");
+    return;
   }
 });
+
+// caclBtn.addEventListener("onmouse",()=>{
+//   alert("over")
+// });
