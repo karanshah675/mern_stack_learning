51c1eb29cff0288c3dbcf835b96bc210593ec84a
karanshah675 <shahkaran0675@gmail.com>
2024-04-28 15:54:31 +0530

    files created

--- javascript/js projects/ageCalculator/script.js
+++ javascript/js projects/ageCalculator/script.js
@@ -13,26 +13,26 @@ date.addEventListener("input", (e) => {
   let d1 = birthDate.getDate();
   let today = new Date();
   let y2 = today.getFullYear();
-  let m2 = today.getMonth()+1;
+  let m2 = today.getMonth() + 1;
   let d2 = today.getDate();
-  let y3,m3,d3;
-  y3=y2-y1
+  let y3, m3, d3;
+  y3 = y2 - y1;
   if (m2 >= m1) {
-      m3=m2-m1;
-  }else{
+    m3 = m2 - m1;
+  } else {
     y3--;
-    m3=12+m2-m1
+    m3 = 12 + m2 - m1;
   }
-if(d2>=d1){
-  d3=d2-d1;
-}else{
-  m3--;
-  d3=getDaysOfMonth(y1,m1,0)+d2-d1;
-}
-if(m3<0){
-  m3=11;
-  y3--;
-}
+  if (d2 >= d1) {
+    d3 = d2 - d1;
+  } else {
+    m3--;
+    d3 = getDaysOfMonth(y1, m1, 0) + d2 - d1;
+  }
+  if (m3 < 0) {
+    m3 = 11;
+    y3--;
+  }
   year.innerHTML = y3;
   month.innerHTML = m3;
   day.innerHTML = d3;
